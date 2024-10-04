ad47e8cb68acce9285f8c3e0ded3124e4d738951
Hananta8 <hananta783@gmail.com>
2024-10-04 16:16:50 +0700

    Delete praktikum2 directory

--- praktikum2/cekNilai.py
+++
@@ -1,5 +0,0 @@
-def examCheck(valueOfExam):
-    if valueOfExam <= 100 and valueOfExam >= 90: return 'Feedback: Sangat baik'
-    elif valueOfExam <= 89 and valueOfExam >= 80: return 'Feedback: Baik'
-    elif valueOfExam <= 79 and valueOfExam >= 70: return 'Feedback: Cukup'
-    elif valueOfExam <= 69 and valueOfExam >= 60: return 'Feedback: Kurang'
--- praktikum2/cekbilangan.py
+++
@@ -1,4 +0,0 @@
-def checkOddEvenNumber(numberCheck):
-    if int(numberCheck) % 2 == 0: return f'{int(numberCheck)} adalah bilangan genap.'
-    elif int(numberCheck) % 2 != 1: return f'{int(numberCheck)} adalah bilangan ganjil.'
-    return f'{numberCheck} bukan angka.'
