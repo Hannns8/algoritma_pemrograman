ab0e3c4ac9085255c346e458112035cd6d62f8a5
Hananta8 <hananta783@gmail.com>
2024-10-07 09:32:48 +0700

    Delete praktikum2 directory

--- praktikum2/cekKesehatan.py
+++
@@ -1,5 +0,0 @@
-def healltyCheck(age, blood):
-    if age >= 60 and blood > 140: return 'Status kesehatan: Tinggi'
-    elif age >= 60 and blood <= 140: return 'Status kesehatan: Normal'
-    elif age < 60 and blood > 130: return 'Status kesehatan: Tinggi'
-    elif age < 60 and blood <= 130: return 'Status kesehatan: Normal'
--- praktikum2/cekbilangan.py
+++
@@ -1,3 +0,0 @@
-def checkOddEvenNumber(numberCheck):
-    if int(numberCheck) % 2 == 0: return f'{int(numberCheck)} adalah bilangan genap.'
-    elif int(numberCheck) % 2 != 0: return f'{int(numberCheck)} adalah bilangan ganjil.'
